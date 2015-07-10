789cfa6880b9c00ae24d889fe4faa6be300b19b9
Li Yiqiang <lyq105@163.com>
2015-07-10 17:38:58 +0800

    add build script.

---
+++ bin/build.sh
@@ -0,0 +1,4 @@
+alias pdfcn='pandoc -S --template=/home/liyq/Templates/mytp/article/pandoc.latex.article.template.cn.tex --latex-engine=xelatex --toc --filter pandoc-citeproc '
+alias pptcn='pandoc -S -t beamer --template=/home/liyq/Templates/mytp/presentation/tp.beamer.cn.tex --latex-engine=xelatex --filter pandoc-citeproc '
+alias docx='pandoc -S --toc --filter pandoc-citeproc '
+
